d9dc154b9691b447a51d621b03c5e361ef37102e
Mario Coronado <chinocoronado2@gmail.com>
2022-08-02 19:54:49 -0300

    incoporar eventos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function loguearcliente () {
+ function loguearcliente () {
 let cliente = prompt ("Bienvenido a nuestra tienda, Por favor ingrese su nombrey apellido")
 console.log(cliente)
 let existe = clientes.find((busqueda) => {
@@ -9,8 +9,7 @@ if (existe){
 else {
     alert("No estas registrado, registrate y vuelve")
 }
-}
-
+} 
 const clientes = ["Mario Coronado", "Post Malone", "Dua Lipa", "Santiago Heria", "Lionel Messi"]
 
 class Discos {
@@ -26,21 +25,29 @@ let disco1 = new Discos ("Harry's house", 2022, "Harry Styles", 10000);
 let disco2 = new Discos ("Folklore", 2020, "Taylor Swift", 8000)
 let disco3 = new Discos ("Equals", 2021, "Ed sheeran", 9000)
 
-function agregarAlCarrito(){
-let productoSelect = prompt ("Que disco le gustaria comprar, 1: Harry's house, 2: Folklore, 3: Equals, 4:Salir")
- console.log (productoSelect)
- if (productoSelect == 1){
-carrito.push(disco1)
-}
-  if (productoSelect == 2){
-    carrito.push(disco2)
-    }
-      if (productoSelect == 3){
-        carrito.push(disco3)
-        }
-}
+
 
 const carrito = [] 
-loguearcliente();
-agregarAlCarrito();
+
+const sendinfo= () => {
+  carrito.push(disco1)}
+
+  const sendinfoT= () => {
+    carrito.push(disco2)}
+
+    const sendinfoF= () => {
+      carrito.push(disco3)}
+
+const input = document.getElementById("send")
+input.addEventListener("click", sendinfo)
+
+const inputT = document.getElementById("sendT")
+input.addEventListener("click", sendinfoT)
+
+const inputF = document.getElementById("sendF")
+input.addEventListener("click", sendinfoF)
+
 console.log(carrito)
+
+loguearcliente();
+
